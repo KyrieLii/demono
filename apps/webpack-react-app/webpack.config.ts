032718f878c07d85dc604fc517411eddbf1c5b64
kyli <KyrieLii@users.noreply.github.com>
2023-03-24 16:13:51 +0800

    feat: cra

---
+++ apps/webpack-react-app/webpack.config.ts
@@ -0,0 +1,17 @@
+import { Configuration } from "webpack";
+
+const config: Configuration = {
+  entry: {},
+  output: {},
+  module: {
+    rules: [
+      {
+        use: "",
+        resolve: {},
+      },
+    ],
+  },
+  devtool: "",
+};
+
+export default config;

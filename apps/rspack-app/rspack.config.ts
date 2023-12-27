15647154dc3cb45e45462c7e516dbc03813ce8dc
kyli <KyrieLii@users.noreply.github.com>
2023-12-27 22:48:17 +0800

    refactor: rspack app to ts

---
+++ apps/rspack-app/rspack.config.ts
@@ -0,0 +1,66 @@
+import { type RspackOptions, rspack } from "@rspack/core";
+import * as ReactRefreshPlugin from "@rspack/plugin-react-refresh";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const config: RspackOptions = {
+  context: __dirname,
+  mode: isDev ? "development" : "production",
+  entry: {
+    main: "./src/main.tsx",
+  },
+
+  plugins: [
+    new rspack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
+    new rspack.ProgressPlugin({}),
+    new rspack.HtmlRspackPlugin({
+      template: "./index.html",
+    }),
+    isDev ? new ReactRefreshPlugin() : null,
+  ].filter(Boolean),
+
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: "asset",
+      },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        use: [
+          {
+            loader: "builtin:swc-loader",
+            options: {
+              sourceMap: true,
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: "automatic",
+                    development: isDev,
+                    refresh: isDev,
+                  },
+                },
+              },
+              env: {
+                targets: [
+                  "chrome >= 87",
+                  "edge >= 88",
+                  "firefox >= 78",
+                  "safari >= 14",
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export default config;
